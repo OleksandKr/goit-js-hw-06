b7720201684629db8d99fe2914e5e2eb5c530894
OleksandKr <cfif357@ukr.net>
2023-04-12 19:04:44 +0300

    1-task

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categorieslistEl = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${categorieslistEl.length}`);
+
+const itemListEl = document.querySelectorAll(`h2`);
+console.log(itemListEl.textContent);
+
+
+
