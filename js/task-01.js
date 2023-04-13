47560390fe6a2548db5fa3ba828e2eab74d23758
OleksandKr <cfif357@ukr.net>
2023-04-13 11:01:02 +0300

    1-task-ok

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,7 @@
 const categorieslistEl = document.querySelectorAll(`.item`);
 console.log(`Number of categories: ${categorieslistEl.length}`);
 
-const itemListEl = document.querySelectorAll(`h2`);
-console.log(itemListEl.textContent);
-
-
-
+categorieslistEl.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
