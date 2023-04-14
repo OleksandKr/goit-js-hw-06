37c415618d394b55a4f22d48690b0b7be1a752e0
OleksandKr <cfif357@ukr.net>
2023-04-14 16:31:23 +0300

    task-4-ok

--- js/task-04.js
+++ js/task-04.js
@@ -1,32 +1,31 @@
-// const counterEl = document.querySelector("#counter");
-// console.log(counterEl);
+let counterValue = 0;
 
-const counterValue = 0;
+const spanEl = document.querySelector("#value");
+console.log(spanEl);
+
 
 const buttonDecrementEl = document.querySelector(
   'button[data-action="decrement"]'
 );
 console.log(buttonDecrementEl);
 
-buttonDecrementEl.addEventListener('click', () => {
-    console.log('---')
-});
-
-
-
-
 const buttonIncrementEl = document.querySelector(
   'button[data-action="increment"]'
 );
 console.log(buttonIncrementEl);
 
-buttonIncrementEl.addEventListener('click', () => {
-    console.log('+++')
 
+
+buttonDecrementEl.addEventListener("click", () => {
+    console.log(counterValue);
+    counterValue -= 1;
+    spanEl.textContent = counterValue;
 });
 
 
-const spanEl = document.querySelector('#value');
-console.log(spanEl);
+buttonIncrementEl.addEventListener('click', () => {
+    console.log(counterValue);
+    counterValue += 1;
+    spanEl.textContent = counterValue;
+});
 
-counterValue = spanEl;
