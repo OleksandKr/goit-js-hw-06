bc892aa03677f4be5308d50d5716bc14518edc6a
OleksandKr <cfif357@ukr.net>
2023-04-19 13:20:16 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,14 @@
 const formEl = document.querySelector(".login-form");
-const userEmailEl = document.querySelector('[type="email"]');
-const passwordEl = document.querySelector('[type="password"]');
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  const elements = {
-    email: userEmailEl.value,
-    pass: passwordEl.value,
-  };
-  if (userEmailEl.value === "" || passwordEl.value === "") {
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value.trim() === "" || password.value.trim() === "") {
     return alert("Заповніть всі поля будь-ласка!");
   }
-    
-  console.log(elements);
-  formEl.reset();
+  const userForm = { email: email.value, password: password.value };
+  console.log(userForm);
+  event.target.reset();
 });
